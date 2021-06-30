9a292a837fce91d144d1f0e1ba275e795f1b3660
vikramvk2003 <vikramvk2003@gmail.com>
2021-06-30 12:09:05 +0530

    final commit

---
+++ app/src/main/java/com/example/memeshare/MainActivity.kt
@@ -0,0 +1,121 @@
+package com.example.memeshare
+
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.StrictMode
+import android.view.View
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+
+class MainActivity : AppCompatActivity() {
+    var currentImageUrl:String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        loadMeme()
+        val builder: StrictMode.VmPolicy.Builder = StrictMode.VmPolicy.Builder()
+        StrictMode.setVmPolicy(builder.build())
+    }
+
+    private fun loadMeme(){
+        // Instantiate the RequestQueue.
+        loader.visibility = View.VISIBLE
+
+        val url = "https://meme-api.herokuapp.com/gimme"
+
+// Request a string response from the provided URL.
+        val JsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET, url,null,
+            { response ->
+                currentImageUrl = response.getString("url")
+
+                Glide.with(this,).load(currentImageUrl).listener(object:RequestListener<Drawable> {
+
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        loader.visibility = View.GONE
+                        return false
+                        TODO("Not yet implemented")
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        loader.visibility = View.GONE
+                        return false
+                        TODO("Not yet implemented")
+                    }
+
+                }).into(imageView)
+
+            },
+            {
+
+            })
+
+// Add the request to the RequestQueue.
+        MySingleton.getInstance(this,).addToRequestQueue(JsonObjectRequest)
+    }
+
+    fun shareMeme(view: View) {
+        shareImageFromURI(currentImageUrl)
+
+    }
+
+    private fun shareImageFromURI(url: String?) {
+        Picasso.get().load(url).into(object : com.squareup.picasso.Target {
+            override fun onBitmapLoaded(bitmap: Bitmap?, from: Picasso.LoadedFrom?) {
+                val intent = Intent(Intent.ACTION_SEND)
+                intent.type = "image/*"
+                intent.putExtra(Intent.EXTRA_STREAM, getBitmapFromView(bitmap))
+                startActivity(Intent.createChooser(intent, "Share Image"))
+            }
+            override fun onPrepareLoad(placeHolderDrawable: Drawable?) { }
+            override fun onBitmapFailed(e: java.lang.Exception?, errorDrawable: Drawable?) { }
+        })
+    }
+
+    fun nextMeme(view: View) {
+        loadMeme()
+    }
+
+    fun getBitmapFromView(bmp: Bitmap?): Uri? {
+        var bmpUri: Uri? = null
+        try {
+            val file = File(this.externalCacheDir, System.currentTimeMillis().toString() + ".jpg")
+
+            val out = FileOutputStream(file)
+            bmp?.compress(Bitmap.CompressFormat.JPEG, 90, out)
+            out.close()
+            bmpUri = Uri.fromFile(file)
+
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return bmpUri
+    }
+}
